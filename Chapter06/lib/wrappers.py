a56ee88ee45db448890ad68bb43774b31541b1a0
Alvaro Henriquez <bigalh94@gmail.com>
2022-11-30 09:35:31 -0500

    Added code to wrappers.py

--- Chapter06/lib/wrappers.py
+++ Chapter06/lib/wrappers.py
@@ -55,4 +55,38 @@ class ProcessFrame84(gym.ObservationWrapper):
     def __init_(self, env=None):
         super(ProcessFrame84, self).__init__(env):
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(84, 48, 1), dtype=np.uint8)
+
+    def observation(self, obs):
+        return ProcessFrame84.process(obs)
+
+    @staticmethod
+    def process(frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160, 3]).astype(np.float)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
+        else:
+            assert False, "Unknown resolution."
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+        x_t = resized_screen[18:102, :]
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        new_shape = (old_shape[-1], old_shape[0], old_shape[1])
+        self.oservation_space = gym.spaces.Box(
+            low=0.0, high=1.0, shape=new_shape, dtype=np.float32
+        )
+
+    def observation(self, observation):
+        return np.moveaxis(observation, 2, 0)
+
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32)  
         
+            
